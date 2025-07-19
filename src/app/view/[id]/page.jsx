fa598e1df4f2782d32e30a2f2c1137ce933557c1
taphead <techne.trax@proton.me>
2025-07-19 15:11:19 +0600

    fixed view

--- src/app/view/[id]/page.jsx
+++ src/app/view/[id]/page.jsx
@@ -2,7 +2,17 @@
 
 import { useParams } from "next/navigation";
 import { useEffect, useState } from "react";
-import Box from "@mui/material/Box";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  Paper,
+  Typography,
+  Box,
+} from "@mui/material";
 
 export default function EditFormPage() {
   const [loading, setLoading] = useState(true);
@@ -22,20 +32,47 @@ export default function EditFormPage() {
   if (loading) return <div>Loading...</div>;
   else
     return (
-      <Box>
-        <Box>{`Name: ` + invoice.customerName}</Box>
-        <Box>{`Invoice Date: ` + invoice.invoiceDate}</Box>
-        <Box>{`Due Date: ` + invoice.dueDate}</Box>
+      <>
         <Box>
-          {invoice.items.map((item, index) => (
-            <Box key={item.itemName}>
-              <div>{`Item ${index + 1}: ` + item.itemName}</div>
-              <div>{`Quantity: ` + item.quantity}</div>
-              <div>{`Unit Price: $` + item.unitPrice}</div>
-              <div>{`Total Price: $` + item.quantity * item.unitPrice}</div>
-            </Box>
-          ))}
+          <Box
+            sx={{
+              display: "flex",
+              flexDirection: "column",
+              justifyContent: "center",
+              alignItems: "center",
+              gap: "0.5rem",
+            }}
+          >
+            <Box>{`Name: ` + invoice.customerName}</Box>
+            <Box>{`Invoice Date: ` + invoice.invoiceDate}</Box>
+            <Box>{`Due Date: ` + invoice.dueDate}</Box>
+          </Box>
+
+          <TableContainer component={Paper} sx={{ mt: 2 }}>
+            <Table>
+              <TableHead>
+                <TableRow>
+                  <TableCell align="center">Item</TableCell>
+                  <TableCell align="center">Quantity</TableCell>
+                  <TableCell align="center">Unit Price</TableCell>
+                  <TableCell align="center">Total Price</TableCell>
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                {invoice.items.map((item, index) => (
+                  <TableRow key={index}>
+                    <TableCell align="center">{item.itemName}</TableCell>
+                    <TableCell align="center"> {item.quantity}</TableCell>
+                    <TableCell align="center">${item.unitPrice}</TableCell>
+                    <TableCell align="center">
+                      ${item.quantity * item.unitPrice}
+                    </TableCell>
+                  </TableRow>
+                ))}
+              </TableBody>
+            </Table>
+          </TableContainer>
         </Box>
-      </Box>
+      </>
     );
 }
